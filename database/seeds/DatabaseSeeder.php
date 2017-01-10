6f1eec5e8c5d77dad73c1f0ff33ea679ca0539c6
Martin <m.vaz.mota@gmail.com>
2017-01-10 16:19:02 +0000

    alteração ordem seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(ListsTableSeeder::class);
         $this->call(ProductsTableSeeder::class);
-        $this->call(ListPivotUsersTableSeeder::class);
         $this->call(UserTableSeeder::class);
+        $this->call(ListPivotUsersTableSeeder::class);
 
     }
 }
