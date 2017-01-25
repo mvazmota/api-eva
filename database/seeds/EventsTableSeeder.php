94bc4e20ae2c5302cedf9a69a8c7c7d70d9425d0
Martin <m.vaz.mota@gmail.com>
2017-01-25 18:25:27 +0000

    update seeds

--- database/seeds/EventsTableSeeder.php
+++ database/seeds/EventsTableSeeder.php
@@ -14,7 +14,8 @@ class EventsTableSeeder extends Seeder
         DB::table('events')->insert([
             'title' => 'Festa da Catarina',
             'date' => '09/01/2017',
-            'time' => '12h30',
+            'start_time' => '12h30',
+            'end_time' => '16h30',
             'location' => 'Aveiro',
             'description' => 'Que festa que vai ser!',
         ]);
@@ -22,7 +23,7 @@ class EventsTableSeeder extends Seeder
         DB::table('events')->insert([
             'title' => 'Festa da Catarina2',
             'date' => '22/01/2017',
-            'time' => '12h30',
+            'start_time' => '12h30',
             'location' => 'Aveiro',
             'description' => 'Que festa que vai ser!',
         ]);
@@ -30,7 +31,7 @@ class EventsTableSeeder extends Seeder
         DB::table('events')->insert([
             'title' => 'Festa da Catarina',
             'date' => '12/02/2017',
-            'time' => '12h30',
+            'start_time' => '12h30',
             'location' => 'Aveiro',
             'description' => 'Que festa que vai ser!',
         ]);
