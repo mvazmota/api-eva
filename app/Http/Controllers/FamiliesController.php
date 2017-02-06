566ee5abd102f3ec7870e1bca4d5cd46613a7cd0
Martin <m.vaz.mota@gmail.com>
2017-02-06 19:05:40 +0000

    middlewere family

--- app/Http/Controllers/FamiliesController.php
+++ app/Http/Controllers/FamiliesController.php
@@ -19,7 +19,7 @@ class FamiliesController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:api');
+        $this->middleware('auth:api', ['except' => ['create']]);
     }
 
     /**
